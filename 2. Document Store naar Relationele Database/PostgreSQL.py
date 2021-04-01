67cbdd242b3a2c4bb14ca3674f02a19d7c96257a
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-01 22:40:55 +0200

    Update PostgreSQL.py

--- 2. Document Store naar Relationele Database/PostgreSQL.py
+++ 2. Document Store naar Relationele Database/PostgreSQL.py
@@ -9,107 +9,136 @@ db = client['huwebshop']
 
 con = psycopg2.connect(
     host="localhost",
-    database="MongoDB",
+    database="huwebshop",
     user="postgres",
     password=" "
 )
 cur = con.cursor()
 
-products = db.products
-data = products.find({})
 
-#33979 / 34004 producten correct
-#uploaden duurt ong 15 sec
-y = 0
-for x in data:
-    id = x['_id']
-    try:
-        name = x['name']
-    except:
-        continue
-    brand = x['brand']
-    category = x['category']
-    color = x['color']
-    description = x['description']
-    gender = x['gender']
-    try:
-        herhaalaankopen = x['herhaalaankopen']
-    except:
-        herhaalaankopen = False
-    price = x['price']['selling_price'] / 100
-    try:
-        discount = x['properties']['discount']
-    except:
-        discount = None
-    try:
-        doelgroep = x['properties']['doelgroep']
-    except:
-        doelgroep = None
-    try:
-        sub_category = x['sub_category']
-    except:
-        sub_category = None
-    try:
-        sub_sub_category = x['sub_sub_category']
-    except:
-        sub_sub_category = None
+def producten_uploaden():
+    products = db.products
+    data = products.find({})
 
-    y += 1
-    print("{} / 33979 ({}%)".format(y, round(y * 100 / 33979, 1)))
+    #33979 / 34004 producten correct
+    count = -1
+    for x in data:
+        count += 1
+        print(count)
 
-    cur.execute("insert into products (product_id, brand, category, color, description, gender, herhaalaankopen, name, price, discount, doelgroep, sub_category, sub_sub_category) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                (id, brand, category, color, description, gender, herhaalaankopen, name, price, discount, doelgroep, sub_category, sub_sub_category))
+        id = x['_id']
+        try:
+            name = x['name']
+        except:
+            continue
+        brand = x['brand']
+        category = x['category']
+        color = x['color']
+        description = x['description']
+        gender = x['gender']
+        try:
+            herhaalaankopen = x['herhaalaankopen']
+        except:
+            herhaalaankopen = False
+        price = x['price']['selling_price'] / 100
+        try:
+            discount = x['properties']['discount']
+        except:
+            discount = None
+        try:
+            doelgroep = x['properties']['doelgroep']
+        except:
+            doelgroep = None
+        try:
+            sub_category = x['sub_category']
+        except:
+            sub_category = None
+        try:
+            sub_sub_category = x['sub_sub_category']
+        except:
+            sub_sub_category = None
 
-visitors = db.visitors
-visitorsdata = visitors.find({})
+        cur.execute(
+            "insert into products (product_id, brand, category, color, description, gender, herhaalaankopen, name, price, discount, doelgroep, sub_category, sub_sub_category) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (id, brand, category, color, description, gender, herhaalaankopen, name, price, discount, doelgroep,
+             sub_category, sub_sub_category))
 
-sessions = db.sessions
-sessionsdata = sessions.find({})
 
-#uploaden van 20000 duurt ong 2.75min
-y = 0
-for i in range(0, 20000):
-    x = visitorsdata[i]
-    z = sessionsdata[i]
+def profiles_uploaden():
+    profiles = db.profiles
+    profilesdata = profiles.find({})
 
-    id = str(x['_id'])
-    try:
-        viewed_before = x['recommendations']['viewed_before']
-        if not viewed_before:
+    count = -1
+    for x in profilesdata:
+        count += 1
+        print(count)
+
+        id = str(x['_id'])
+        try:
+            buids = x['buids']
+            if not buids:
+                buids = None
+        except:
+            buids = None
+        try:
+            viewed_before = x['recommendations']['viewed_before']
+            if not viewed_before:
+                viewed_before = None
+        except:
             viewed_before = None
-    except:
-        viewed_before = None
-    try:
-        similars = x['recommendations']['similars']
-        if not similars:
+        try:
+            similars = x['recommendations']['similars']
+            if not similars:
+                similars = None
+        except:
             similars = None
-    except:
-        similars = None
-    try:
-        previously_recommended = x['previously_recommended']
-        if not previously_recommended:
+        try:
+            previously_recommended = x['previously_recommended']
+            if not previously_recommended:
+                previously_recommended = None
+        except:
             previously_recommended = None
-    except:
-        previously_recommended = None
 
-    cur.execute("insert into visitors (visitor_id, viewed_before, similars, previously_recommended) values (%s, %s, %s, %s)",
-                (id, viewed_before, similars, previously_recommended))
+        cur.execute(
+            "insert into profiles (profile_id, buids, viewed_before, similars, previously_recommended) values (%s, %s, %s, %s, %s)",
+            (id, buids, viewed_before, similars, previously_recommended))
+        con.commit()
 
-    id = z['_id']
-    session_start = z['session_start']
-    session_end = z['session_end']
-    has_sale = z['has_sale']
-    try:
-        order_products = str(z['order']['products'])
-    except:
-        order_products = None
 
-    y += 1
-    print("{} / 20000 ({}%)".format(y, round(y * 100 / 20000), 1))
+def sessions_uploaden():
+    sessions = db.sessions
+    sessionsdata = sessions.find({})
 
-    cur.execute(
-        "insert into sessions (session_id, session_start, session_end, has_sale, order_products) values (%s, %s, %s, %s, %s)",
-        (id, session_start, session_end, has_sale, order_products))
+    count = -1
+    for x in sessionsdata:
+        count += 1
+        print(count)
+
+        id = str(x['_id']) if (type(x['_id']) is not str) else x['_id']
+        try:
+            buid = x['buid'][0] if (len(x['buid']) == 1 and type(x['buid'][0]) is str) else (
+                x['buid'][0][0] if (len(x['buid']) == 1) else (
+                    x['buid'] if (type(x['buid'][0]) is str and type(x['buid'][1]) is str) else (
+                        [x['buid'][0], x['buid'][1][0]] if (type(x['buid'][0]) is str) else (
+                        [x['buid'][0][0], x['buid'][1]]))))
+        except:
+            buid = None
+        session_start = x['session_start']
+        session_end = x['session_end']
+        has_sale = x['has_sale']
+        try:
+            order_products = str(x['order']['products'])
+        except:
+            order_products = None
+
+        cur.execute(
+            "insert into sessions (session_id, buid, session_start, session_end, has_sale, order_products) values (%s, %s, %s, %s, %s, %s)",
+            (id, buid, session_start, session_end, has_sale, order_products))
+        con.commit()
+
+
+profiles_uploaden()
+sessions_uploaden()
 
 client.close()
 
