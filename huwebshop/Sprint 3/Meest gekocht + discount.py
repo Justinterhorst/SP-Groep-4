1cb77331ebcbbd96537bc6e030f4577aee49948e
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-07 16:52:35 +0200

    Update Meest gekocht + discount.py
    Docstrings toegevoegd

--- huwebshop/Sprint 3/Meest gekocht + discount.py
+++ huwebshop/Sprint 3/Meest gekocht + discount.py
@@ -8,6 +8,9 @@ def meestgekocht(con):
     discounts = []
     for x in cur.fetchall():
         discounts.append(x[0])
+    """
+    Alle discounts in een lijst zetten zodat er later gekeken kan worden of een id discount heeft
+    """
 
     cur.execute("select order_products from sessions where order_products is not null")
     order_products = cur.fetchall()
@@ -30,12 +33,21 @@ def meestgekocht(con):
                         products.append(y[0][9:len(y[0])-3:])
         count += 1
         print("{}/185113".format(count))
+    """
+    Query hierboven haalt alle gekochte producten op. De loop gaat elk gekochte product langs om na te gaan of het een
+    discount heeft. Zo ja, wordt het (met wat modificatie om de data goed te kunnen gebruiken) in een lijst gezet. 
+    """
 
     meestgekocht = []
     for i in range(0, 4):
         meestvoorkomende = max(set(products), key=products.count)
         products = [x for x in products if x != meestvoorkomende]
         meestgekocht.append(meestvoorkomende)
+    """
+    Hier worden de 4 meest voorkomende vastgesteld. Eerst wordt er gekeken welk product het meeste voorkomt, vervolgens
+    wordt die uit de samengekocht lijst verwijderd, en als laatste wordt het meest voorkomende id aan een nieuwe lijst 
+    toegevoegd.
+    """
 
     print(meestgekocht)
 
