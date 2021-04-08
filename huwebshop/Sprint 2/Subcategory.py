0d9e45f442fbfc801cec18d7518a950090a61dad
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-08 15:21:08 +0200

    Update Subcategory.py
    Globale variabelen weggehaald.
    Code verbeterd.
    Order_products wordt nu uit relationele database gehaald voor efficiëntie

--- huwebshop/Sprint 2/Subcategory.py
+++ huwebshop/Sprint 2/Subcategory.py
@@ -1,79 +1,65 @@
 import psycopg2
 
-con = psycopg2.connect(
-    host="localhost",
-    database="huwebshop",
-    user="postgres",
-    password=" "
-)
-cur = con.cursor()
-
 
 def betere_recommendation(data):
     if data is None:
         return None
     else:
-        if len(set(data)) < 5:
-            aantal = len(set(data))
-        elif len(set(data)) >= 5:
-            aantal = 5
-
-        lijst = []
-        for i in range(0, aantal):
-            meestvoorkomende = max(set(data), key=data.count)
-            data = [x for x in data if x != meestvoorkomende]
-            lijst.append(meestvoorkomende)
-        if not lijst:
+        if len(set(data)) >= 4:
+            lijst = []
+            for i in range(0, 4):
+                meestvoorkomende = max(set(data), key=data.count)
+                data = [x for x in data if x != meestvoorkomende]
+                lijst.append(meestvoorkomende)
+        elif len(set(data)) != 0:
+            lijst = list(set(data))
+        elif len(set(data)) == 0:
             lijst = None
         return lijst
 
 
-def subcategory():
+def subcategory(con):
+    cur = con.cursor()
+    
     cur.execute("select sub_category from products where sub_category is not null")
     subcategories = list(set(cur.fetchall()))
 
-    cur.execute("select viewed_before from visitors where viewed_before is not null")
-    viewed_before = cur.fetchall()
+    cur.execute("select viewed_before from profiles where viewed_before is not null")
+    viewed_before = []
+    for y in cur.fetchall():
+        if "," in y[0][1:len(y[0]) - 1:]:
+            for z in y[0][1:len(y[0]) - 1:].split(","):
+                viewed_before.append(z)
+        else:
+            viewed_before.append(y[0][1:len(y[0]) - 1:])
 
-    cur.execute("select order_products from sessions where has_sale is true and order_products is not null")
-    order_products = cur.fetchall()
+    cur.execute("select data from lijsten")
+    order_products = (cur.fetchone()[0].strip('{}')).split(',')
 
-
-    count = 0
     for x in subcategories:
+        count = 0
         bekekenproducten = []
         for y in viewed_before:
-            if "," in y[0][1:len(y[0])-1:]:
-                for z in y[0][1:len(y[0])-1:].split(","):
-                    cur.execute("select sub_category from products where product_id = '{}'".format(z))
-                    subcategory = cur.fetchone()
-                    if subcategory is None:
-                        continue
-                    elif subcategory[0] == x[0]:
-                        bekekenproducten.append(z)
-            else:
-                cur.execute("select sub_category from products where product_id = '{}'".format(y[0][1:len(y[0])-1:]))
-                subcategory = cur.fetchone()
-                if subcategory[0] == x[0]:
-                    bekekenproducten.append(y[0][1:len(y[0])-1:])
+            cur.execute("select subcategory from products where product_id = '{}'".format(y))
+            subcategory = cur.fetchone()
+            if subcategory is not None and subcategory[0] == x:
+                bekekenproducten.append(y)
+            count += 1
+            print("{} / 1464507".format(count))
         if not bekekenproducten:
             bekekenproducten = None
 
         meestbekeken = betere_recommendation(bekekenproducten)
 
+        count = 0
         gekochteproducten = []
         for y in order_products:
-            if "," in y[0][9:len(y[0]) - 3:]:
-                for z in y[0][0:len(y[0]) - 1:].split(","):
-                    cur.execute("select sub_category from products where product_id = '{}'".format(z[9:len(z) - 2:]))
-                    subcategory = cur.fetchone()
-                    if subcategory[0] == x[0]:
-                        gekochteproducten.append(z[9:len(z) - 2:])
-            else:
-                cur.execute("select sub_category from products where product_id = '{}'".format(y[0][9:len(y[0]) - 3:]))
-                subcategory = cur.fetchone()
-                if subcategory[0] == x[0]:
-                    gekochteproducten.append(y[0][9:len(y) - 4:])
+            cur.execute("select subcategory from products where product_id = '{}'".format(y))
+            subcategory = cur.fetchone()
+            if subcategory is not None and subcategory[0] == x:
+                gekochteproducten.append(y)
+            count += 1
+            print("{} / 658960".format(count))
         if not gekochteproducten:
             gekochteproducten = None
 
@@ -81,13 +67,14 @@ def subcategory():
 
         cur.execute("insert into subcategory (subcategory, bekekenproducten, gekochteproducten, meestbekeken, meestgekocht) values (%s, %s, %s, %s, %s)",
                     (x[0], bekekenproducten, gekochteproducten, meestbekeken, meestgekocht))
+        con.commit()
+    cur.close()
+    con.close()
 
-        count += 1
-        print("{} / {}   ({:.1f}%)".format(count, 54, count * 100 / 54))
 
-
-subcategory()
-
-con.commit()
-cur.close()
-con.close()
+subcategory(psycopg2.connect(
+    host="localhost",
+    database="huwebshop",
+    user="postgres",
+    password=" "
+))
